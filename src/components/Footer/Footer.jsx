d97a0bbbbeaf8ab0c1d21810ab00bb2f514ad9a9
SandraMey <sandra.meyniel@gmail.com>
2022-01-19 17:40:00 +0100

    add zone & footer & header

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import imgFooter from '../../assets/footermano.png'
+import './footer.css'
+
+const Footer = () => {
+  return <footer className="footer">
+      <img src={imgFooter} alt="" />
+  </footer>;
+};
+
+export default Footer
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+
+const HomePage = () => (
+  <div className="home">
+    <h1>Home Page</h1>
+
+  </div>
+);
+
+export default HomePage;
