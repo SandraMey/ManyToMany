b112efa9e61fc8b0892e17b1ca35fa05238205cc
SandraMey <sandra.meyniel@gmail.com>
2022-01-20 18:29:29 +0100

    add hover effect work

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, useState } from "react";
 import ButtonChoice from "../ButtonChoice/buton";
 import imgplumbing from "../../assets/plumbing.jpg";
 import imgtapis from "../../assets/tapis.webp";
@@ -7,6 +7,14 @@ import { Link } from "react-router-dom";
 import "./work.css";
 
 const Work = () => {
+  const [color, setcolor] = useState(true);
+  const [color1, setcolor1] = useState(true);
+  const [color2, setcolor2] = useState(true);
+  const [color3, setcolor3] = useState(true);
+  const [color4, setcolor4] = useState(true);
+  const [color5, setcolor5] = useState(true);
+  console.log(color);
+  
   return (
     <main className="work">
       <h1>What type of work is involved?</h1>
@@ -14,16 +22,16 @@ const Work = () => {
         <p>2/5</p>
       </div>
       <div className="containerwork">
-        <div className="containertitle">
+        <div className="containertitle" onClick ={() => setcolor(!color)}>
           <ButtonChoice
             choice={"faucets"}
             style={`${"faucets"}`}
             linkTo="/work"
             svg={<img src={imgplumbing} alt="" className="imgplumbing"></img>}
           />
-          <p className="titlework">Plumbing</p>
+          <p className={color ? "titlework" : "hovertitlework"}>Plumbing</p>
         </div>
-        <div className="containertitle">
+        <div className="containertitle" onClick ={() => setcolor1(!color1)}>
           <ButtonChoice
             choice={"shower"}
             style={`${"shower"}`}
@@ -36,9 +44,9 @@ const Work = () => {
               ></img>
             }
           />
-          <p className="titlework">Shower</p>
+          <p className={color1 ? "titlework" : "hovertitlework"}>Shower</p>
         </div>
-        <div className="containertitle">
+        <div className="containertitle" onClick ={() => setcolor2(!color2)}>
           <ButtonChoice
             choice={"SPA"}
             style={`${"SPA"}`}
@@ -51,27 +59,27 @@ const Work = () => {
               ></img>
             }
           />
-          <p className="titlework">Baths</p>
+          <p className={color2 ? "titlework" : "hovertitlework"}>Baths</p>
         </div>
-        <div className="containertitle">
+        <div className="containertitle" onClick ={() => setcolor3(!color3)}>
           <ButtonChoice
             choice={"floor"}
             style={`${"floor"}`}
             linkTo="/work"
             svg={<img src={imgfloor} alt="" className="imgplumbing"></img>}
           />
-          <p className="titlework">Floor</p>
+          <p className={color3 ? "titlework" : "hovertitlework"}>Floor</p>
         </div>
-        <div className="containertitle">
+        <div className="containertitle" onClick ={() => setcolor4(!color4)}>
           <ButtonChoice
             choice={"accesories"}
             style={`${"accesories"}`}
             linkTo="/work"
             svg={<img src={imgtapis} alt="" className="imgplumbing"></img>}
           />
-          <p className="titlework">Acessories</p>
+          <p className={color4 ? "titlework" : "hovertitlework"}>Acessories</p>
         </div>
-        <div className="containertitle">
+        <div className="containertitle" onClick ={() => setcolor5(!color5)}>
           <ButtonChoice
             choice={"wc"}
             style={`${"wc"}`}
@@ -84,15 +92,13 @@ const Work = () => {
               ></img>
             }
           />
-          <p className="titlework">Toilets</p>
+          <p className={color5 ? "titlework" : "hovertitlework"}>Toilets</p>
         </div>
       </div>
-      <div className="buttonprev">
+      <div className="buttonconfirm">
         <Link to="/">
           <button className="button">Prev</button>
         </Link>
-      </div>
-      <div className="buttonnext">
         <Link to="/deepening">
           <button className="button">Next</button>
         </Link>
